41867954fc2c6ca20847d68a237ccfc0f6f56429
Jesus Anaya <jesus.anaya.developer@gmail.com>
2023-05-26 22:56:01 -0600

    maquetacion completade card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListCitasComponent } from './components/list-citas/list-citas.component';
 import { CrearCitasComponent } from './components/crear-citas/crear-citas.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { TimepickerModule } from 'ngx-bootstrap/timepicker';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { CrearCitasComponent } from './components/crear-citas/crear-citas.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BsDatepickerModule.forRoot(),
+    TimepickerModule.forRoot(),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/crear-citas/crear-citas.component.ts
+++ src/app/components/crear-citas/crear-citas.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-crear-citas',
   templateUrl: './crear-citas.component.html',
-  styleUrls: ['./crear-citas.component.css']
+  styleUrls: ['./crear-citas.component.css'],
+
 })
 export class CrearCitasComponent {
-
+  mytime: Date = new Date();
 }
